9340293ad4580a9ff0c22296211d37e164da22d4
Alexandre Sales Medina Ferreira <94998770+alexmedinasf@users.noreply.github.com>
2022-11-04 20:44:57 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
