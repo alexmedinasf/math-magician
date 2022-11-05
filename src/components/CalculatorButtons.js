1883c677a8b4ce225d5ccde151eedcb5a2ebb6f0
alexmedinasf <alexsales001@hotmail.com>
2022-11-05 10:37:30 -0300

    Fixed the multiplication button

--- src/components/CalculatorButtons.js
+++ src/components/CalculatorButtons.js
@@ -43,7 +43,7 @@ CalculatorButtons.propTypes = {
 CalculatorButtons.defaultProps = {
   buttons: [
     'AC', '+/-', '%', '÷',
-    '7', '8', '9', '×',
+    '7', '8', '9', 'x',
     '4', '5', '6', '-',
     '1', '2', '3', '+',
     '0', '.', '=',
