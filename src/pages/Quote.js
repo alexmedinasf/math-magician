c7331be02fac429b015ec0e78df152299b4084af
alexmedinasf <alexsales001@hotmail.com>
2022-11-09 10:56:02 -0300

    minor changes

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -6,14 +6,6 @@ const Quote = () => {
       quote: 'Just because we can’t find a solution, it doesn’t mean there isn’t one.',
       author: 'Andrew Wiles, English mathematician',
     },
-    {
-      quote: 'Mathematics is a place where you can do things which you can’t do in the real world.',
-      author: 'Marcus du Sautoy',
-    },
-    {
-      quote: 'Millions saw the apple fall, but Newton asked why.',
-      author: 'Bernard Baruch, American financier, philanthropist and statesman',
-    },
   ];
 
   const randomQuote = () => {
