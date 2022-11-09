576bf501b37a73bb478f118df9c2b67588bd3958
alexmedinasf <alexsales001@hotmail.com>
2022-11-09 11:06:05 -0300

    added router to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Navbar from './components/Navbar';
+import NoMatch from './pages/NoMatch';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = '';
-  }
-
-  render() {
-    return <Calculator />;
-  }
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App;
