14b51ac5a46af401a5fed8c18d8aac14528e2840
alexmedinasf <alexsales001@hotmail.com>
2022-11-09 11:18:15 -0300

    Linters fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/quote" element={<Quote />} />
-        <Route path="*" element={<NoMatch />} />
       </Routes>
     </Router>
   );
